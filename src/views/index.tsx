d7b01c3570af99d6020d6e24c9c176d0df94db2e
Adam Laycock <adam@arcath.net>
2018-01-15 13:56:43 +0000

    improve the index

--- src/views/index.tsx
+++ src/views/index.tsx
@@ -3,16 +3,19 @@ import {Link} from 'react-router-dom'
 
 export const Index = () => (
   <div className="index">
-    <ul>
-      <li>
-        <Link to='/building-planner'>Building Planner</Link>
-      </li>
-      <li>
-        <Link to='/creep-designer'>Creep Designer</Link>
-      </li>
-      <li>
-        <Link to='/memory-viewer'>Memory Viewer</Link>
-      </li>
-    </ul>
+    <h2><Link to='/building-planner'>Building Planner</Link></h2>
+    <p>
+      The Building planner is a port of Dissi's Building planner into React.
+      
+      All of its api calls are passed through an express instance on the screeps-tolls server to avoid CORS issues.
+    </p>
+    <h2><Link to='/creep-designer'>Creep Designer</Link></h2>
+    <p>
+      The Creep Designer gives a UI to build creeps and see all their stats.
+    </p>
+    <h2><Link to='/memory-viewer'>Memory Viewer</Link></h2>
+    <p>
+      The Memory viewer pulls a copy of your Memory from any of the shards. This can then be viewed in React-JSON-View and queiried using JQ.
+    </p>
   </div>
 )
